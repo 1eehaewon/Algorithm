a65e0487ccc52b644d94df868797938862fb1042
1eehaewon <145750695+1eehaewon@users.noreply.github.com>
2025-05-31 17:30:26 +0900

    Time: 93 ms (23.9%), Space: 46.5 MB (32.73%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -7,6 +7,7 @@ class Solution {
         Stack<Character> stack = new Stack<>();
 
         // 스택에 문자 넣기
+        /* 굳이 *을 넣었다 뺄 필요 없음
         for(int i=0; i<s.length(); i++) {
             stack.push(s.charAt(i));
             if(s.charAt(i) == '*') {
@@ -14,16 +15,28 @@ class Solution {
                 stack.pop();
             }
         }
+        */
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '*'){
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
         // 스택에서 꺼낸 후 역순으로 해야함 -> 다른 스택에 삽입
         Stack<Character> reverse = new Stack<>();
         while(!stack.isEmpty()){
             reverse.push(stack.pop());
         }
         // reverse에서 차례대로 꺼내서 문자열로 만들어야함
-        String result = "";
+        // String result = ""; String은 매번 객체가 생성돼서 성능 저하 이슈
+        StringBuilder result = new StringBuilder();
         while(!reverse.isEmpty()){
-            result += reverse.pop();
+            // result += reverse.pop();
+            result.append(reverse.pop());
         }
-        return result;
+        // return result;
+        return result.toString();
     }
 }
