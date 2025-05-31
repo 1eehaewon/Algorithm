470c673de47b89e681bb5b8d7fcae084bfdc0819
1eehaewon <145750695+1eehaewon@users.noreply.github.com>
2025-05-31 15:52:45 +0900

    Time: 2 ms (98.68%), Space: 42.2 MB (34.23%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        // 배열 중 숫자의 개수가 각각 다르면 true
+        // HashMap -> key, value -> key = 배열 숫자, value = 숫자의 개수
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 배열의 각 숫자의 개수 - (1,3) (2,2) (3,1)
+        for(int i=0; i<arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        
+        // Map길이만큼 돌면서 각 value 비교 (3) (2) (1)
+        // -> Set에 value를 넣었을 때, 길이가 줄어들면 중복이 존재
+        Set<Integer> set = new HashSet<>(map.values());
+        if(map.size() != set.size()) {
+            return false;
+        }
+        return true;
+    }
+}
